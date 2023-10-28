66df07c661d9f3b565110283d8c9734c92900ee3
sanjeevgaddi <sanjeevgaddi7@gmail.com>
2023-10-28 12:17:56 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function Greet() {
+import './index.css'
+
+const BookList = () => {
   return (
-    <>
-      <h3>My First Component</h3>;
-    </>
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
+}
+
+const Book = () => {
+  const title = 'Forever Strong: A New, Science-Based Strategy for Agingggggggg'
+  const author = 'Dr. Gabrielle Lyon'
+  return (
+    <article className="book">
+      <img src="./images/book-1.jpg" alt="{title}" />,<h2>{title}</h2>,
+      <h4>{author}</h4>
+    </article>
   )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Greet />)
+root.render(<BookList />)
